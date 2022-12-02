a2de1395666e962d777968070e38b6374035f0cd
Ishan Pranav <ishan.pranav@outlook.com>
2022-12-02 00:35:06 -0500

    Add project files.

---
+++ src/Advent/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Advent.StreamProviders;
+
+namespace Advent
+{
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            IStreamProvider provider = new FileStreamProvider();
+
+            int day1Part1 = await Day1Part1Solution.SolveAsync(provider);
+            int day1Part2 = await Day1Part2Solution.SolveAsync(provider);
+
+            await Console.Out.WriteLineAsync($"Day 1, Part 1: {day1Part1}");
+            await Console.Out.WriteLineAsync($"Day 1, Part 2: {day1Part2}");
+        }
+    }
+}
---
+++ src/Advent/StreamProviders/IStreamProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Advent.StreamProviders
+{
+    internal interface IStreamProvider
+    {
+        Task<StreamReader> OpenTextAsync(int day);
+    }
+}
